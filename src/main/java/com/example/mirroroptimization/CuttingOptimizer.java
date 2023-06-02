ec82ecb60edac62de6346b1f267555ad6d2d0cad
ask0 <voidask0@gmail.com>
2023-06-03 02:44:07 +0300

    CuttingOptimizer updated

--- src/main/java/com/example/mirroroptimization/CuttingOptimizer.java
+++ src/main/java/com/example/mirroroptimization/CuttingOptimizer.java
@@ -1,75 +1,51 @@
 package com.example.mirroroptimization;
 
-import java.util.*;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import javafx.scene.control.Alert;
 
 public class CuttingOptimizer {
     private MirrorPlate plate;
-    private List<MirrorPiece> pieces;
 
     public CuttingOptimizer(MirrorPlate plate) {
         this.plate = plate;
-        this.pieces = plate.getPieces();
     }
+
     public void placePieces() {
-        sortPiecesByAreaDescending();
+        int x = 0, y = 0;
 
-        for (MirrorPiece piece : pieces) {
-            if (!placePiece(piece)) {
-                throw new RuntimeException("Cannot place all pieces without overlapping.");
-            }
-        }
-        sortPiecesByPosition();
-    }
+        for (MirrorPiece piece : plate.getPieces()) {
+            // Parçayı yerleştir
+            piece.setPosX(x);
+            piece.setPosY(y);
 
+            // Diğer tüm parçalarla çakışıp çakışmadığını kontrol et
+            for (MirrorPiece otherPiece : plate.getPieces()) {
+                if (piece != otherPiece && piece.isOverlap(otherPiece)) {
+                    // Çakışma varsa, parçayı sağa hareket ettir ve çakışmayı kontrol etmeye devam et
+                    x += otherPiece.getWidth();
+                    piece.setPosX(x);
 
-    private boolean placePiece(MirrorPiece piece) {
-        for (int y = 0; y <= plate.getHeight() - piece.getHeight(); y++) {
-            for (int x = 0; x <= plate.getWidth() - piece.getWidth(); x++) {
-                if (tryPlacePiece(piece, x, y)) {
-                    return true;
+                    // Eğer parça plaka sınırlarını aşıyorsa, bir alt satıra geç ve x koordinatını sıfırla
+                    if (x + piece.getWidth() > plate.getWidth()) {
+                        x = 0;
+                        y += otherPiece.getHeight();
+                        piece.setPosX(x);
+                        piece.setPosY(y);
+                    }
+
+                    // Yeni parça için yer ayır
+                    x += piece.getWidth();
                 }
             }
-        }
-        return false;
-    }
 
-    private void sortPiecesByPosition() {
-        pieces.sort(Comparator.comparingInt(MirrorPiece::getPosY).thenComparingInt(MirrorPiece::getPosX));
-    }
-
-
-    private boolean tryPlacePiece(MirrorPiece piece, int x, int y) {
-        if (x + piece.getWidth() > plate.getWidth() || y + piece.getHeight() > plate.getHeight()) {
-            return false;
-        }
-
-        for (MirrorPiece existingPiece : pieces) {
-            if (existingPiece != piece && intersect(piece, x, y, existingPiece)) {
-                return false;
+            // Eğer parça plaka sınırlarını aşıyorsa, hata ver ve döngüyü kır
+            if (x + piece.getWidth() > plate.getWidth() || y + piece.getHeight() > plate.getHeight()) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Hata");
+                alert.setHeaderText(null);
+                alert.setContentText("Ayna parçaları plaka sınırlarını aştı!");
+                alert.showAndWait();
+                break;
             }
         }
-        piece.setPosX(x);
-        piece.setPosY(y);
-        return true;
-    }
-
-    private boolean intersect(MirrorPiece piece, int x, int y, MirrorPiece existingPiece) {
-        int left1 = x;
-        int right1 = x + piece.getWidth();
-        int top1 = y;
-        int bottom1 = y + piece.getHeight();
-
-        int left2 = existingPiece.getPosX();
-        int right2 = existingPiece.getPosX() + existingPiece.getWidth();
-        int top2 = existingPiece.getPosY();
-        int bottom2 = existingPiece.getPosY() + existingPiece.getHeight();
-        return left1 < right2 && right1 > left2 && top1 < bottom2 && bottom1 > top2;
-    }
-
-    private void sortPiecesByAreaDescending() {
-        pieces.sort(Comparator.comparingInt(MirrorPiece::getArea).reversed());
     }
 }
